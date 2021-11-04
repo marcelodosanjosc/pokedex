28444de3a1de7579545224d5abb2535146999d83
marcelodosanjosc <marcelodosanjosc@gmail.com>
2021-11-04 16:35:58 -0400

    Criação da palicação pokedex em angular

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
+import { PokeHeaderComponent } from './poke-header/poke-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
+  ],
+  exports:[
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
+  ],
+  imports: [
+    CommonModule, 
+    RouterModule
+  ]
+})
+export class SharedModule { }
